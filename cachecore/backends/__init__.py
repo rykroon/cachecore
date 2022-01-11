aaac90557804539dea18d8ceaf45103430fd4ef1
Ryan Kroon <rykroon.tech@gmail.com>
2022-01-10 22:26:42 -0500

    Added FileBackend.

--- cachecore/backends/__init__.py
+++ cachecore/backends/__init__.py
@@ -1,4 +1,5 @@
 from cachecore.backends.base import BaseBackend
 from cachecore.backends.dummy import DummyBackend
+from cachecore.backends.file import FileBackend
 from cachecore.backends.local import LocalBackend
 from cachecore.backends.redis import RedisBackend
