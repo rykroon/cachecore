afead62422331402707dc432e19847dca997c18d
Ryan Kroon <rykroon.tech@gmail.com>
2022-01-10 22:27:58 -0500

    Added _get, _set, _del, get, set, add, delete, methods.

--- cachecore/backends/file.py
+++ cachecore/backends/file.py
@@ -3,35 +3,42 @@ from hashlib import md5
 
 from cachecore.backends.base import BaseBackend
 from cachecore.serializers import PickleSerializer
-from cachecore.utils import Value
+from cachecore.utils import MissingKey, Value
 
 
 class FileBackend(BaseBackend):
+
+    ext = '.cachecore'
     
-    def __init__(self, dir, suffix='cache'):
+    def __init__(self, dir):
         self.serializer = PickleSerializer()
         self._dir = os.path.abspath(dir)
-        self._suffix = suffix
         os.makedirs(self._dir, mode=0o700, exist_ok=True)
 
     def _key_to_file(self, key):
         fname = md5(key.encode(), usedforsecurity=False).hexdigest()
-        fname += self._suffix
+        fname += self.ext
         return os.path.join(self._dir, fname)
 
-    def get(self, key):
-        ...
+    def _get(self, key):
+        fname = self._key_to_file(key)
+        try:
+            with open(fname, 'rb') as f:
+                value = self.serializer.load(f)
+                if value.is_expired():
+                    # delete file
+                    return MissingKey
+                return value
 
-    def set(self, key, value, ttl):
-        v = Value(value, ttl)
+        except FileNotFoundError:
+            return MissingKey
+
+    def _set(self, key, value):
         fname = self._key_to_file(key)
         with open(fname, 'wb') as f:
-            self.serializer.dump(f)
+            self.serializer.dump(value, f)
 
-    def add(self, key, value, ttl):
-        ...
-
-    def delete(self, key):
+    def _del(self, key):
         fname = self._key_to_file(key)
         if not os.path.exists(fname):
             return False
@@ -42,5 +49,24 @@ class FileBackend(BaseBackend):
             return False
         return True
 
+    def get(self, key):
+        value = self._get(key)
+        if value is MissingKey:
+            return MissingKey
+        return value.value
+
+    def set(self, key, value, ttl):
+        self._set(key, Value(value, ttl))
+
+    def add(self, key, value, ttl):
+        if self.has_key(key):
+            return False
+        self.set(key, value, ttl)
+        return True
+
+    def delete(self, key):
+        return self._del(key)
+
     def has_key(self, key):
-        ...
+        value = self._get(key)
+        return value is not MissingKey
