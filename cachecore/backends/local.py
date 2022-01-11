6592e4f1eae8c96b293549c28bdd2f473d4df94f
Ryan Kroon <rykroon.tech@gmail.com>
2022-01-10 21:05:06 -0500

    Refactored local backend.

--- cachecore/backends/local.py
+++ cachecore/backends/local.py
@@ -1,6 +1,7 @@
+from typing import Union
 from cachecore.backends.base import BaseBackend
 from cachecore.serializers import PickleSerializer
-from cachecore.utils import MissingKey, Value
+from cachecore.utils import MissingKey, MissingKeyType, Value
 
 
 class LocalBackend(BaseBackend):
@@ -9,7 +10,7 @@ class LocalBackend(BaseBackend):
         self.serializer = PickleSerializer()
         self.data = {}
 
-    def _get_value(self, key):
+    def _get(self, key: str) -> Union[Value, MissingKeyType]:
         value = self.data.get(key)
         if value is None:
             return MissingKey
@@ -17,19 +18,25 @@ class LocalBackend(BaseBackend):
         value = self.serializer.loads(value)
         
         if value.is_expired():
-            del self.data[key]
+            self._del(key)
             return MissingKey
 
         return value
 
+    def _set(self, key: str, value: Value):
+        self.data[key] = self.serializer.dumps(value)
+
+    def _del(self, key: str) -> bool:
+        return self.data.pop(key, MissingKey) is not MissingKey
+
     def get(self, key):
-        value = self._get_value(key)
+        value = self._get(key)
         if value is MissingKey:
             return MissingKey
-        return self.serializer.loads(value.value)
+        return value.value
 
     def set(self, key, value, ttl):
-        self.data[key] = self.serializer.dumps(Value(value, ttl))
+        self._set(key, Value(value, ttl))
 
     def add(self, key, value, ttl):
         if self.has_key(key):
@@ -38,15 +45,10 @@ class LocalBackend(BaseBackend):
         return True
 
     def delete(self, key):
-        value = self._get_value(key)
-        if value is MissingKey:
-            return False
-
-        del self.data[key]
-        return True
+        return self._del(key)
 
     def has_key(self, key):
-        value = self._get_value(key)
+        value = self._get(key)
         return value is not MissingKey
 
     def get_many(self, *keys):
@@ -60,15 +62,16 @@ class LocalBackend(BaseBackend):
         return [self.delete(k) for k in keys]
 
     def get_ttl(self, key):
-        value = self._get_value(key)
+        value = self._get(key)
         if value is MissingKey:
             return 0
         return value.get_ttl()
 
     def set_ttl(self, key, ttl):
-        value = self._get_value(key)
+        value = self._get(key)
         if value is not MissingKey:
             value.set_ttl(ttl)
+            self._set(key, value)
 
     def clear(self):
         self.data.clear()
