f55374117da8f5a5f3b79ce84e82e5a78db8bfe6
Ryan Kroon <rykroon.tech@gmail.com>
2022-01-15 13:50:52 -0500

    renamed _get to _read_value and _set to _write_value. Added additional methods that implement the Backend Interface.

--- cachecore/backends/file.py
+++ cachecore/backends/file.py
@@ -7,20 +7,19 @@ from cachecore.utils import MissingKey, Value
 
 
 class FileBackend(BaseBackend):
-
-    ext = '.cachecore'
     
-    def __init__(self, dir):
+    def __init__(self, dir, ext='.cachecore'):
         self.serializer = PickleSerializer()
         self._dir = os.path.abspath(dir)
+        self._ext = ext
         os.makedirs(self._dir, mode=0o700, exist_ok=True)
 
     def _key_to_file(self, key):
         fname = md5(key.encode(), usedforsecurity=False).hexdigest()
-        fname += self.ext
+        fname += self._ext
         return os.path.join(self._dir, fname)
 
-    def _get(self, key):
+    def _read_value(self, key):
         fname = self._key_to_file(key)
         try:
             with open(fname, 'rb') as f:
@@ -33,12 +32,27 @@ class FileBackend(BaseBackend):
         except FileNotFoundError:
             return MissingKey
 
-    def _set(self, key, value):
+    def _write_value(self, key, value):
         fname = self._key_to_file(key)
         with open(fname, 'wb') as f:
             self.serializer.dump(value, f)
 
-    def _del(self, key):
+    def get(self, key):
+        value = self._read_value(key)
+        if value is MissingKey:
+            return MissingKey
+        return value.value
+
+    def set(self, key, value, ttl):
+        self._write_value(key, Value(value, ttl))
+
+    def add(self, key, value, ttl):
+        if self.has_key(key):
+            return False
+        self.set(key, value, ttl)
+        return True
+
+    def delete(self, key):
         fname = self._key_to_file(key)
         if not os.path.exists(fname):
             return False
@@ -49,24 +63,28 @@ class FileBackend(BaseBackend):
             return False
         return True
 
-    def get(self, key):
-        value = self._get(key)
-        if value is MissingKey:
-            return MissingKey
-        return value.value
-
-    def set(self, key, value, ttl):
-        self._set(key, Value(value, ttl))
-
-    def add(self, key, value, ttl):
-        if self.has_key(key):
-            return False
-        self.set(key, value, ttl)
-        return True
-
-    def delete(self, key):
-        return self._del(key)
-
     def has_key(self, key):
-        value = self._get(key)
-        return value is not MissingKey
+        fname = self._key_to_file(key)
+        return os.path.isfile(fname)
+
+    def get_many(self, *keys):
+        return [self.get(k) for k in keys]
+
+    def set_many(self, mapping, ttl):
+        for k, v in mapping.items():
+            self.set(k, v, ttl)
+
+    def delete_many(self, *keys):
+        return [self.delete(k) for k in keys]
+
+    def get_ttl(self, key):
+        value = self._read_value(key)
+        if value is MissingKey:
+            return 0
+        return value.get_ttl()
+
+    def set_ttl(self, key, ttl):
+        value = self._read_value(key)
+        if value is not MissingKey:
+            value.set_ttl(ttl)
+            self._write_value(key, value)
