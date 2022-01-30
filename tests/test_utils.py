1356965b82639f525374acbe009a122fb3c026f8
Ryan Kroon <rykroon.tech@gmail.com>
2022-01-30 17:01:20 -0500

    Added test cases for everything in utils.py

---
+++ tests/test_utils.py
@@ -0,0 +1,48 @@
+import os
+import unittest
+import time
+
+from cachecore.utils import Value, MISSING_KEY, MissingKey, Directory
+
+
+class TestMissingKey(unittest.TestCase):
+    def test_missing_key(self):
+        assert MISSING_KEY is MissingKey()
+
+
+class TestValue(unittest.TestCase):
+    def test_value(self):
+        v = Value(10, None)
+        assert v.get_ttl() is None
+        assert v.is_expired() is False
+
+        v.set_ttl(1)
+        assert v.get_ttl() == 1
+        time.sleep(1)
+        assert v.get_ttl() == 0
+        assert v.is_expired() is True
+
+
+class TestDirectory(unittest.TestCase):
+    def setUp(self):
+        home_dir = os.environ['HOME']
+        path = os.path.join(home_dir, 'cachecore-tests')
+        self.dir = Directory(path)
+
+    def test_directory(self):
+        self.dir['a.txt'] = b'hello world'
+        assert self.dir['a.txt'] == b'hello world'
+        assert ('a.txt' in self.dir) is True
+        del self.dir['a.txt']
+
+        with self.assertRaises(KeyError):
+            self.dir['a.txt']
+
+        with self.assertRaises(KeyError):
+            del self.dir['a.txt']
+
+        assert ('a.txt' in self.dir) is False
+
+
+if __name__ == '__main__':
+    unittest.main()
