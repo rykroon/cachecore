1bc9f46384de283476a1368be6e70e1d213909a1
Ryan Kroon <rykroon.tech@gmail.com>
2022-02-26 16:02:55 -0500

    Removed Directory class.

--- src/cachecore/utils.py
+++ src/cachecore/utils.py
@@ -1,6 +1,4 @@
-from functools import cache
 from math import ceil
-from pathlib import Path
 import time
 
 
@@ -18,49 +16,3 @@ def ttl_remaining(exp_time):
 
 def is_expired(exp_time):
     return ttl_remaining(exp_time) == 0
-
-
-class Directory:
-
-    def __init__(self, dir):
-        self.path = Path(dir)
-        if not self.path.is_absolute():
-            self.path = self.path.absolute()
-
-        if self.path.exists() and not self.path.is_dir():
-            raise Exception
-
-        if not self.path.exists():
-            self.path.mkdir()
-
-    def __getitem__(self, key):
-        f = self.path / key
-        try:
-            return f.read_bytes()
-        except FileNotFoundError:
-            raise KeyError(key)
-
-    def __setitem__(self, key, value):
-        f = self.path / key
-        f.write_bytes(value)
-
-    def __delitem__(self, key):
-        f = self.path / key
-        try:
-            f.unlink()
-        except FileNotFoundError:
-            raise KeyError(key)
-
-    def __contains__(self, key):
-        f = self.path / key
-        return f.exists()
-
-    def __iter__(self):
-        return self.path.iterdir()
-
-
-def singleton(class_):
-    class_.__new__ = cache(class_.__new__)
-    return class_
-
-
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,28 +1,4 @@
-import os
 import unittest
-
-from cachecore.utils import Directory
-
-
-class TestDirectory(unittest.TestCase):
-    def setUp(self):
-        home_dir = os.environ['HOME']
-        path = os.path.join(home_dir, 'cachecore-tests')
-        self.dir = Directory(path)
-
-    def test_directory(self):
-        self.dir['a.txt'] = b'hello world'
-        assert self.dir['a.txt'] == b'hello world'
-        assert ('a.txt' in self.dir) is True
-        del self.dir['a.txt']
-
-        with self.assertRaises(KeyError):
-            self.dir['a.txt']
-
-        with self.assertRaises(KeyError):
-            del self.dir['a.txt']
-
-        assert ('a.txt' in self.dir) is False
 
 
 if __name__ == '__main__':
