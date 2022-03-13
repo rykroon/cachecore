f7c1d705ae7cb9849276d6aea994f0fd29dfdcb6
Ryan Kroon <rykroon.tech@gmail.com>
2022-03-13 19:59:37 -0400

    Removed comments.

--- src/cachecore/__init__.py
+++ src/cachecore/__init__.py
@@ -1,16 +1,1 @@
 from .caches import *
-
-
-"""
-    Features to add.
-
-    - pop(), like delete(), but returns the value that was deleted.
-
-    - replace(), like add(), but only sets the key if it already exists.
-
-    - __iter__(): Iterate over keys
-
-    - __len__: Return length of keys.
-
-
-"""
