9d25045e66d5662dde3d31662572569869d99bc4
Ryan Kroon <rykroon.tech@gmail.com>
2022-05-27 10:38:27 -0400

    Added docs/ directory.

---
+++ docs/source/conf.py
@@ -0,0 +1,37 @@
+from pallets_sphinx_themes import get_version
+
+
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information
+
+project = 'cachecore'
+copyright = '20212 Ryan Kroon'
+author = 'Ryan Kroon'
+
+release, version = get_version("cachecore")
+
+# -- General configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
+templates_path = ['_templates']
+
+# -- Options for HTML output
+
+html_theme = 'sphinx_rtd_theme'
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
