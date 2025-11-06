2d20aea5b5bd3ebd07f64aa1a8ee4a72315adb24
Ryan Kroon <rykroon.tech@gmail.com>
2025-11-06 08:02:04 -0500

    Recreated project structure as a fresh hatch project.

---
+++ README.md
@@ -0,0 +1,21 @@
+# cachecore
+
+[![PyPI - Version](https://img.shields.io/pypi/v/cachecore.svg)](https://pypi.org/project/cachecore)
+[![PyPI - Python Version](https://img.shields.io/pypi/pyversions/cachecore.svg)](https://pypi.org/project/cachecore)
+
+-----
+
+## Table of Contents
+
+- [Installation](#installation)
+- [License](#license)
+
+## Installation
+
+```console
+pip install cachecore
+```
+
+## License
+
+`cachecore` is distributed under the terms of the [MIT](https://spdx.org/licenses/MIT.html) license.
--- src/cachecore/__init__.py
+++ src/cachecore/__init__.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: 2025-present Ryan Kroon <rykroon.tech@gmail.com>
+#
+# SPDX-License-Identifier: MIT
 from .interface import CacheInterface
 from .base import BaseCache
 from .dummy import DummyCache
@@ -5,4 +8,18 @@ from .file import FileCache
 from .local import LocalCache
 from .memcached import MemcachedCache
 from .redis import RedisCache
+
+
+"""
+NOTE
+- use hatch for package management
+- greatly simplify
+  - only define get, set, del, has (__contains__), size (__len__), clear.
+  - maybe pop and popitem
+  - See Mapping and Mutable Mapping (https://docs.python.org/3/library/collections.abc.html)
+  - fuck memcached
+  - valkey instead of redis?
+
+"""
+
 
