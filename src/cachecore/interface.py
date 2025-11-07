fe0ecfe60248069995f2007585fd53647a264143
Ryan Kroon <rykroon.tech@gmail.com>
2025-11-07 18:46:42 -0500

    new interface based on redis commands.

--- src/cachecore/interface.py
+++ src/cachecore/interface.py
@@ -10,17 +10,17 @@ class CacheInterface:
         pass
 
     @abstractmethod
-    def set(self, key: str, value: Any, ttl: int | None = None):
+    def set(self, key: str, value: Any, ex: int | None = None):
         """Assign a value to a key.
 
         :param key: The key to be set.
         :param value: The value to be stored.
-        :param ttl: The time-to-live. If None, then the value will not expire.
+        :param ex: The time-to-live. If None, then the value will not expire.
         """
         pass
 
     @abstractmethod
-    def add(self, key: str, value: Any, ttl: int | None = None) -> bool:
+    def add(self, key: str, value: Any, ex: int | None = None) -> bool:
         """Set the value only if the key doesn't already exist.
 
         :param key: The key to be set.
@@ -31,57 +31,26 @@ class CacheInterface:
         pass
 
     @abstractmethod
-    def delete(self, key: str) -> bool:
+    def replace(self, key: str, value: Any) -> bool:
         pass
 
     @abstractmethod
-    def has(self, key: str) -> bool:
+    def delete(self, *keys: str) -> int:
         pass
 
     @abstractmethod
-    def get_or_set(self, key: str, value: Any, ttl: int | None = None):
+    def exists(self, *keys: str) -> int:
+        pass
+
+    def __contains__(self, key: str) -> bool:
+        return self.exists(key) == 1
+    
+    @abstractmethod
+    def ttl(self, key: str) -> int:
         pass
 
     @abstractmethod
-    def get_many(self, keys: Iterable[str], default: Any = None) -> Iterable[Any]:
-        """Returns an iterable of values.
-
-        :param keys: An iterable of keys to retrieve.
-        :param default: A default value in case a key is not found.
-        :returns: An iterable of values.
-        """
-        pass
-
-    @abstractmethod
-    def set_many(self, mapping: Iterable[tuple[str, Any]], ttl: int | None = None):
-        """Stores the mapping of key value pairs.
-
-        :param mapping: An iterable of key, value tuples.
-        :param ttl: The time-to-live.
-        """
-        pass
-
-    @abstractmethod
-    def delete_many(self, keys: Iterable[str]) -> Iterable[bool]:
-        """Deletes all of the keys in the iterable.
-
-        :param keys: An iterable of keys to be deleted.
-        :returns: An iterable of boolean values indicating if the key was deleted.
-        """
-        pass
-
-    @abstractmethod
-    def touch(self, key: str, ttl: int | None = None) -> bool:
-        """Sets the TTL of the key.
-
-        :param key: The key.
-        :param ttl: The time-to-live.
-        :returns: True, if the TTL was updated, else False.
-        """
-        pass
-
-    @abstractmethod
-    def incr(self, key, delta=1) -> int:
+    def incr(self, key, amount: int = 1) -> int:
         """Increment the value associated with the key.
         Creates the key if it does not exist.
 
@@ -92,7 +61,7 @@ class CacheInterface:
         pass
 
     @abstractmethod
-    def decr(self, key, delta=1) -> int:
+    def decr(self, key, amount: int = 1) -> int:
         """Decrement the value associated with the key.
         Creates the key if it does not exist.
 
