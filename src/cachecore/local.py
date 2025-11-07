f848d7988a3e8656b0b7c197ddb40234f995704e
Ryan Kroon <rykroon.tech@gmail.com>
2025-11-07 18:49:39 -0500

    Created ExpiryDict to use for DictCache.

--- src/cachecore/local.py
+++ src/cachecore/local.py
@@ -1,67 +1,36 @@
 from typing import Any, Iterable, Mapping
 
-from .utils import ExpiryValue, _missing_key
+from .utils import ExpiryValue, ExpiryDict, _missing_key
 
 
-class LocalCache:
+class DictCache:
 
     def __init__(self):
-        self._data: dict[str, ExpiryValue] = {}
-
-    def _get(self, key: str) -> ExpiryValue | object:
-        ev = self._data.get(key)
-        if ev is None:
-            return _missing_key
-
-        if ev.is_expired():
-            del self._data[key]
-            return _missing_key
-
-        return ev
+        self._data: ExpiryDict = ExpiryDict()
 
     def get(self, key: str, default=None):
-        ev = self._get(key)
-        if ev is _missing_key:
-            return default
-        return ev.value
+        return self._data.get(key, default)
 
     def set(self, key: str, value: Any, ttl: int | None = None):
-        self._data[key] = ExpiryValue(value, ttl)
+        self._data.set(key, value, ttl)
 
-    def delete(self, key: str) -> bool:
-        ev = self._get(key)
-        if ev is _missing_key:
-            return False
+    def delete(self, *keys: str) -> int:
+        return sum(self._data.pop(k, _missing_key) is not _missing_key for k in keys)
 
-        del self._data[key]
-        return False if ev.is_expired() else True
-
-    def has_key(self, key: str) -> bool:
-        return self._get(key) is not _missing_key
+    def exists(self, *keys: str) -> int:
+        return sum(k in self._data for k in keys)
 
     def __contains__(self, key: str) -> bool:
-        return self.has_key(key)
+        return key in self._data
 
     def add(self, key: str, value: Any, ttl: int | None = None) -> bool:
-        if key not in self._data:
+        if key not in self:
             self.set(key, value, ttl)
             return True
         return False
 
-    def touch(self, key: str, ttl: int | None = None) -> bool:
-        ev = self._get(key)
-        if ev is _missing_key or ev.is_expired():
-            return False
- 
-        ev.ttl = ttl
-        return True
-
-    def get_or_set(self, key: str, default: Any, ttl: int | None = None) -> Any:
-        ev = self._get(key)
-        if ev is _missing_key:
-            self.set(key, default, ttl)
-            return default
-        return ev.value
+    def replace(self, key: str, value: Any):
+        pass
 
     def get_many(self, keys: Iterable[str]) -> dict[str, Any]:
         return {
@@ -75,18 +44,9 @@ class LocalCache:
         self._data.update(values)
         return []
 
-    def delete_many(self, keys: Iterable[str]):
-        for key in keys:
-            self.delete(key)
-
     def incr(self, key: str, delta: int = 1) -> int:
-        ev = self._get(key)
-        if ev is _missing_key:
-            raise ValueError("key not found")
+        pass
 
-        ev.value += delta
-        return ev.value
-    
     def decr(self, key: str, delta: int = 1) -> int:
         return self.incr(key, -delta)
 
--- src/cachecore/utils.py
+++ src/cachecore/utils.py
@@ -1,3 +1,4 @@
+from collections.abc import MutableMapping
 from dataclasses import dataclass, field, InitVar
 from functools import cache
 from math import ceil
@@ -15,7 +16,10 @@ class ExpiryValue:
     expires_at: float = field(init=False)
 
     def __post_init__(self, ttl: int | None):
-        self.set_ttl(ttl)
+        self.ttl = ttl
+    
+    def __repr__(self):
+        return "EXPIRED" if self.is_expired() else repr(self.value)
 
     @property
     def ttl(self) -> int | None:
@@ -23,12 +27,48 @@ class ExpiryValue:
             return None
         return max(0, ceil(self.expires_at - time.time()))
 
-    def set_ttl(self, ttl: int | None):
+    @ttl.setter
+    def ttl(self, ttl: int | None):
         expiry = float("inf") if ttl is None else ttl
         self.expires_at = time.time() + expiry
 
     def is_expired(self) -> bool:
         return time.time() > self.expires_at
+
+
+class ExpiryDict(MutableMapping[str, ExpiryValue]):
+
+    def __init__(self):
+        self._data: dict[str, ExpiryValue] = {}
+
+    def __repr__(self):
+        return repr(self._data)
+
+    def __getitem__(self, key):
+        item = self._data.get(key)
+        if item is None or item.is_expired():
+            self._data.pop(key, None)
+            raise KeyError(key)
+        return item.value
+
+    def set(self, key: str, value: Any, ttl: int | None = None):
+        self[key] = ExpiryValue(value, ttl)
+
+    def __setitem__(self, key: str, item: ExpiryValue):
+        self._data[key] = item
+
+    def __delitem__(self, key: str):
+        key in self # removes key if expired
+        del self._data[key]
+
+    def __iter__(self):
+        for key in list(self._data.keys()):
+            if key in self:
+                yield key
+
+    def __len__(self):
+        list(iter(self)) # Remove all expired keys
+        return len(self._data)
 
 
 class Singleton:
