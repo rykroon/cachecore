4336ad2ad1ddc5da5cfc0f20937ec242b12ca871
Ryan Kroon <rykroon.tech@gmail.com>
2025-11-06 21:50:29 -0500

    Refactored LocalCache.

--- src/cachecore/local.py
+++ src/cachecore/local.py
@@ -1,90 +1,94 @@
-import pickle
-import typing as t
+from typing import Any, Iterable, Mapping
 
-from .base import BaseCache
-from .utils import KEEP_TTL, ExpiryValue
+from .utils import ExpiryValue, _missing_key
 
 
-class LocalCache(BaseCache):
-
-    serializer = pickle
+class LocalCache:
 
     def __init__(self):
         self._data: dict[str, ExpiryValue] = {}
 
-    def __getitem__(self, key: str) -> t.Any:
-        expval = self._get(key)
-        if expval is None:
-            raise KeyError(key)
-        return self.serializer.loads(expval.value)
+    def _get(self, key: str) -> ExpiryValue | object:
+        ev = self._data.get(key)
+        if ev is None:
+            return _missing_key
 
-    def __setitem__(self, key: str, value: t.Any):
-        self._set(key, value)
+        if ev.is_expired():
+            del self._data[key]
+            return _missing_key
 
-    def __delitem__(self, key: str) -> None:
-        self._get(key) # delete key if it has expired.
+        return ev
+
+    def get(self, key: str, default=None):
+        ev = self._get(key)
+        if ev is _missing_key:
+            return default
+        return ev.value
+
+    def set(self, key: str, value: Any, ttl: int | None = None):
+        self._data[key] = ExpiryValue(value, ttl)
+
+    def delete(self, key: str) -> bool:
+        ev = self._get(key)
+        if ev is _missing_key:
+            return False
+
         del self._data[key]
+        return False if ev.is_expired() else True
+
+    def has_key(self, key: str) -> bool:
+        return self._get(key) is not _missing_key
 
     def __contains__(self, key: str) -> bool:
-        return self._get(key) is not None
+        return self.has_key(key)
 
-    def __iter__(self) -> t.Iterable:
-        for k, v in tuple(self._data.items()):
-            if v.is_expired():
-                del self._data[k]
-                continue
-            yield k
+    def add(self, key: str, value: Any, ttl: int | None = None) -> bool:
+        if key not in self._data:
+            self.set(key, value, ttl)
+            return True
+        return False
 
-    def _get(self, key: str):
-        expval = self._data.get(key)
-        if expval is None:
-            return None
-
-        if expval.is_expired():
-            del self._data[key]
-            return None
-
-        return expval
-
-    def _set(self, key: str, value: t.Any, ttl: int | None = None) -> None:
-        value = self.serializer.dumps(value)
-        expval = ExpiryValue(value=value)
-        expval.ttl = ttl
-        self._data[key] = expval
-
-    def set(self, key: str, value: t.Any, ttl: int | None = None):
-        self._set(key, value, ttl)
-
-    def replace(self, key: str, value: int | None, ttl: int | None = KEEP_TTL):
-        expval = self._get(key)
-        if expval is None:
+    def touch(self, key: str, ttl: int | None = None) -> bool:
+        ev = self._get(key)
+        if ev is _missing_key or ev.is_expired():
             return False
-
-        expval.value = self.serializer.dumps(value)
-        if ttl is not KEEP_TTL:
-            expval.ttl = ttl
+ 
+        ev.ttl = ttl
         return True
 
-    def get_ttl(self, key: str, default: int = 0) -> int:
-        expval = self._get(key)
-        if expval is None:
+    def get_or_set(self, key: str, default: Any, ttl: int | None = None) -> Any:
+        ev = self._get(key)
+        if ev is _missing_key:
+            self.set(key, default, ttl)
             return default
-        return expval.ttl
+        return ev.value
 
-    def set_ttl(self, key: str, ttl: int | None = None) -> bool:
-        expval = self._get(key)
-        if expval is None:
-            return False
+    def get_many(self, keys: Iterable[str]) -> dict[str, Any]:
+        return {
+            k: ev.value
+            for k, ev in self._data.items()
+            if k in keys and not ev.is_expired()
+        }
 
-        expval.ttl = ttl
-        return True
+    def set_many(self, data: Mapping, ttl: int | None = None) -> list[str]:
+        values = {k: ExpiryValue(v, ttl) for k, v in data.items()}
+        self._data.update(values)
+        return []
+
+    def delete_many(self, keys: Iterable[str]):
+        for key in keys:
+            self.delete(key)
 
     def incr(self, key: str, delta: int = 1) -> int:
-        value = self.get(key, default=0)
-        value += delta
-        if not self.add(key, value):
-            self.replace(key, value)
-        return value
+        ev = self._get(key)
+        if ev is _missing_key:
+            raise ValueError("key not found")
+
+        ev.value += delta
+        return ev.value
+    
+    def decr(self, key: str, delta: int = 1) -> int:
+        return self.incr(key, -delta)
 
     def clear(self) -> None:
         self._data.clear()
--- src/cachecore/utils.py
+++ src/cachecore/utils.py
@@ -1,41 +1,34 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field, InitVar
 from functools import cache
 from math import ceil
 import time
-from typing import Optional, Any
+from typing import Any
 
 
-def ttl_to_exptime(ttl: int | None) -> float | None:
-    if ttl is None:
-        return None
-    return time.time() + ttl
-
-
-def ttl_remaining(exp_time: float | None) -> int | None:
-    if exp_time is None:
-        return None
-    return max(0, ceil(exp_time - time.time()))
-
-
-def is_expired(exp_time: float | None) -> bool:
-    return ttl_remaining(exp_time) == 0
+_missing_key = object()
 
 
 @dataclass(slots=True)
 class ExpiryValue:
     value: Any
-    expires_at: Optional[float] = None
+    ttl: InitVar[int | None]
+    expires_at: float = field(init=False)
+
+    def __post_init__(self, ttl: int | None):
+        self.set_ttl(ttl)
 
     @property
-    def ttl(self):
-        return ttl_remaining(self.expires_at)
+    def ttl(self) -> int | None:
+        if self.expires_at == float("inf"):
+            return None
+        return max(0, ceil(self.expires_at - time.time()))
 
-    @ttl.setter
-    def ttl(self, value):
-        self.expires_at = ttl_to_exptime(value)
+    def set_ttl(self, ttl: int | None):
+        expiry = float("inf") if ttl is None else ttl
+        self.expires_at = time.time() + expiry
 
-    def is_expired(self):
-        return is_expired(self.expires_at)
+    def is_expired(self) -> bool:
+        return time.time() > self.expires_at
 
 
 class Singleton:
